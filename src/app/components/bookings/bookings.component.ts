b48c0b01e4ef2c5e9139af38baa120970e03b232
buhleqampi <buhle3857@gmail.com>
2024-11-05 00:35:00 +0200

    bookings data received successfully

--- src/app/components/bookings/bookings.component.ts
+++ src/app/components/bookings/bookings.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { BookingsService } from '../../services/bookings.service';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.component.html',
   styleUrl: './bookings.component.css'
-})
+  })
 export class BookingsComponent {
+  bookings: any;
 
-}
+  constructor(private bookingService: BookingsService) {}
+
+  ngOnInit(): void {
+    this.getBookingData()
+  }
+
+  getBookingData(){
+
+  this.bookingService.getAllBookings().subscribe({
+  next: (res_data)=>{
+   //localStorage.length
+  console.log("Bookings Data Retrieved!! ",res_data)
+  this.bookings = res_data.error_code
+  },
+  error: (err)=>{
+  console.error("Could get bookings!", err)
+
+  }
+
+  }) 
+
+  }
+  }
+
--- src/app/services/unit-type.service.ts
+++ src/app/services/unit-type.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -6,26 +7,24 @@ import { Observable } from 'rxjs';
 })
 export class UnitTypeService {
 
-  private apiUrl = 'https://demo.ix.cimsoweb.com/unit_type_info_request';
+  private apiUrl = "api/unit_type_info_request";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getUnitTypeInfo(): Observable<any> {
-    return new Observable(observer => {
-      fetch(this.apiUrl, { method: 'GET' })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then(data => {
-          observer.next(data);
-          observer.complete();
-        })
-        .catch(error => {
-          observer.error(error);
-        });
+
+    const body = { hg_code: "demo", payload: {} }
+
+    const headers = new HttpHeaders({
+      'Authorization': JSON.stringify({
+        "Client Login ID": "CiMSO.dev",
+        "Client Password": "CiMSO.dev",
+        "hg_pass": "nGXUF1i^57I^ao^o",
+      }),
+      'Content-Type': 'application/json',
+
     });
+
+    return this.http.post(this.apiUrl,body, {headers})
   }
 }
