804b43e082cbf257e5662004aa5ee69bce2d8e9f
buhleqampi <buhle3857@gmail.com>
2024-11-05 15:46:55 +0200

    tailwind css correctly configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FlowbiteService } from './services/flowbite.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Task';
+export class AppComponent implements OnInit {
+ 
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
 }
