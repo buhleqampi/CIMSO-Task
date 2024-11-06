56a43b5d04bea3bce0ea7747bd417315557ad323
buhleqampi <buhle3857@gmail.com>
2024-11-06 13:57:40 +0200

    i can see images

--- src/app/components/unit-type/unit-type.component.ts
+++ src/app/components/unit-type/unit-type.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UnitTypeService } from '../../services/unit-type.service';
 import { Unittype } from '../../interfaces/unittype';
 import { ActivatedRoute } from '@angular/router';
+import { BinaryRequestService } from '../../services/binary-request.service';
 
 @Component({
   selector: 'app-unit-type',
@@ -12,7 +13,9 @@ export class UnitTypeComponent implements OnInit {
 
   unitTypes: Unittype[] = [];
 
-  constructor(private unitTypeService: UnitTypeService, private route: ActivatedRoute) {}
+  
+
+  constructor(private unitTypeService: UnitTypeService,  private binaryRequestService: BinaryRequestService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.getUnitTypes();
@@ -23,18 +26,50 @@ export class UnitTypeComponent implements OnInit {
     console.log(id);
   }
 
+  // getUnitTypes() {
+  //   this.unitTypeService.getUnitTypeInfo().subscribe({
+  //     next: (res_data) => {
+  //       console.log("Unit Type Info Data Retrieved!!! ", res_data.payload["Unit Types"]);
+  //       this.unitTypes = res_data.payload["Unit Types"]; 
+  //     },
+  //     error: (err) => {
+  //       console.error('Error fetching unit type info:', err);
+  //     }
+  //   });
+  // }
   getUnitTypes() {
     this.unitTypeService.getUnitTypeInfo().subscribe({
       next: (res_data) => {
-        console.log("Unit Type Info Data Retrieved!!! ", res_data.payload["Unit Types"]);
-        this.unitTypes = res_data.payload["Unit Types"]; 
+        this.unitTypes = res_data.payload["Unit Types"];
+        this.unitTypes.forEach((unitType) => {
+          if (unitType['Unit Type Image UIDs'] && unitType['Unit Type Image UIDs'].length > 0) {
+            this.fetchImagesForUnitType(unitType);
+          }
+        });
       },
       error: (err) => {
         console.error('Error fetching unit type info:', err);
       }
     });
   }
+
+fetchImagesForUnitType(unitType: Unittype) {
+  unitType.images = unitType.images || []; 
+  unitType['Unit Type Image UIDs'].forEach((imageUID: string) => {
+    this.binaryRequestService.getBinaryRequest(imageUID).subscribe({
+      next: (response: any) => {
+        const imageUrl = `data:image/jpeg;base64,${response}`;
+        unitType.images!.push(imageUrl); 
+      },
+      error: (error) => {
+        console.error('Error fetching binary request for UID:', imageUID, error);
+      }
+    });
+  });
 }
+
+}
+
 
 
 
--- src/app/interfaces/unittype.ts
+++ src/app/interfaces/unittype.ts
@@ -1,18 +1,17 @@
 export interface Unittype {
 
-    "Unit Type ID": number;
-    "Unit Type Code": string;
-    "Unit Type Description": string;
-    "Unit Type Category": string;
-    "Maximum Occupants": number;
-    "Maximum Adults": number;
-    "Maximum Children": number;
-    "Marketing Description": string
-    "Location ID": number;
-    "Unit Count": number;
-    "Unit Type Image UIDs": string[];
-
-  }
+  'Unit Type ID': string;
+  'Unit Type Code': string;
+  'Unit Type Description': string;
+  'Unit Type Category': string;
+  'Maximum Occupants': number;
+  'Maximum Adults': number;
+  'Maximum Children': number;
+  'Marketing Description': string;
+  'Location ID': string;
+  'Unit Count': number;
+  'Unit Type Image UIDs': string[];
+  images?: string[];
   
- 
+}
 
